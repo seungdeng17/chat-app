9af683fa9a14d91c17be9c13cee0b19d76b09b06
sgdg <seungdeng17@gmail.com>
2020-08-20 16:06:44 +0900

    feat: 사용자, 채널 정보 관련 서브 함수 추가

---
+++ server/users.js
@@ -0,0 +1,29 @@
+const users = [];
+
+const addUser = ({ id, name, channel }) => {
+    name = name.trim().toLowerCase();
+    channel = channel.trim().toLowerCase();
+
+    const existingUser = users.find((user) => user.channel === channel && user.name === name);
+
+    if (!name || !channel) return { error: '이름과 채널을 입력하세요.' };
+    if (existingUser) return { error: '채널에 중복된 이름이 있어요.' };
+
+    const user = { id, name, channel };
+
+    users.push(user);
+
+    return { user };
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) return users.splice(index, 1)[0];
+}
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInChannel = (channel) => users.filter((user) => user.channel === channel);
+
+module.exports = { addUser, removeUser, getUser, getUsersInChannel };
