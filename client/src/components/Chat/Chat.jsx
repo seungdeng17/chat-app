7035ca0a7d40a7b4134f7e179bb835548d027550
sgdg <seungdeng17@gmail.com>
2020-08-20 16:07:25 +0900

    feat: 메세지 출력 컴포넌트 구조 작성

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import styled from "styled-components";
 import io from "socket.io-client";
+
+import ChatInfoBar from "./ChatInfoBar";
+import MessageList from "./MessageList";
+import ChatInput from "./ChatInput";
+
+const ChatOuter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const ChatInner = styled.form`
+  width: 400px;
+  height: 700px;
+  border: 1px solid #000;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 let socket;
 
 const Chat = () => {
+  const history = useHistory();
   const { name, channel } = useParams();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "http://localhost:5000";
 
   useEffect(() => {
@@ -14,11 +40,39 @@ const Chat = () => {
     socket.emit("join", { name, channel }, (error) => {
       if (error) {
         alert(error);
+        history.push("/");
       }
     });
-  }, [ENDPOINT, name, channel]);
 
-  return <div></div>;
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, name, channel, history]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <ChatOuter>
+      <ChatInner>
+        <ChatInfoBar />
+        <MessageList />
+        <ChatInput />
+      </ChatInner>
+    </ChatOuter>
+  );
 };
 
 export default Chat;
---
+++ client/src/components/Chat/ChatInfoBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatInfoBar = () => {
+  return <div></div>;
+};
+
+export default ChatInfoBar;
