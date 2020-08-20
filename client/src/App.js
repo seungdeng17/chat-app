d5000a5673e13f973b9dfe065ae120c57409b762
sgdg <seungdeng17@gmail.com>
2020-08-20 14:24:28 +0900

    feat: 글로벌 스타일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import GlobalStyles from '@/styles/GlobalStyles';
+import Join from '@components/Join';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Join} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
