158ccaa4096d9f6db445528ade2490bbee2ed753
sgdg <seungdeng17@gmail.com>
2020-08-20 16:06:12 +0900

    feat: 클라이언트에 메세지 객체 보내는 기능 추가

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUsersInChannel } = require('./users');
+
 const router = require('./router');
 
 const app = express();
@@ -16,9 +18,31 @@ app.use(router);
 
 io.on('connection', (socket) => {
     socket.on('join', ({ name, channel }, callback) => {
-        console.log(name, channel);
-    });
-});
+        const { error, user } = addUser({ id: socket.id, name, channel });
 
+        if (error) return callback(error);
+
+        socket.join(user.channel);
+
+        socket.emit('message', { user: `${user.channel} - Admin`, text: `${user.name}님, 환영합니다.` });
+        socket.broadcast.to(user.channel).emit('message', { user: `${user.channel} - Admin`, text: `${user.name}님이 대화에 참여했습니다.` });
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.channel).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.channel).emit('message', { user: `${user.channel} - Admin`, text: `${user.name}님이 퇴장했습니다.` });
+        }
+    })
+});
 
 server.listen(PORT, () => console.log('Server has started'));
